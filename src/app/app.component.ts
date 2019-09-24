433e0887e7a3fec84b2f4cca3243800f2b612b8f
Josue <josgar_14@hotmail.com>
2019-09-24 17:59:03 -0400

    reactive form with multiple emails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'dynamic-form-controls';
+	title = 'dynamic-form-controls';
+	formModel = new FormGroup({
+		emails: new FormArray([ new FormControl() ])
+	});
+
+	get emailsFormArray() {
+		return this.formModel.get('emails') as FormArray;
+	}
+	get value() {
+		return JSON.stringify(this.formModel.value, null, 4);
+	}
+
+	addEmail() {
+		const emails = this.emailsFormArray;
+		emails.push(new FormControl());
+	}
+
+	onSubmit() {
+		console.log(this.formModel.value);
+	}
 }
